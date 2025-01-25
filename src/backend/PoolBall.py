c3286d71f64e67119d38b3a7599a7a8fe4c10c82
Jacob <Jacob.leader06@gmail.com>
2025-01-25 14:21:53 -0500

    Update PoolBall.py

---
+++ src/backend/PoolBall.py
@@ -0,0 +1,8 @@
+
+class PoolBall:
+    def __init__(self, x_cord, y_cord, color, suit):
+        self.x_cord = x_cord
+        self.y_cord = y_cord
+        self.color = color
+        self.suit = suit
+        
