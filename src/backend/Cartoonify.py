1a48a2d3b11cff0bef796635abb9be66a4f49507
Jacob <Jacob.leader06@gmail.com>
2025-01-26 05:33:04 -0500

    add pool table bg, create pockets

--- src/backend/Cartoonify.py
+++ src/backend/Cartoonify.py
@@ -3,9 +3,9 @@ import numpy as np
 from PoolBall import PoolBall
 
 def preprocess(img):
+  
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-   
     return blurred
 
 def plotCircles(img, circles):
@@ -41,16 +41,17 @@ def plotCircles(img, circles):
     
     return img, pool_balls, avg_radius
 
-def displayBalls(pool_balls, table_coordinates, img_shape, radius):
-    img = np.ones((img_shape[0], img_shape[1], 3), dtype=np.uint8) * 255  # Blank white
-
-    top_right, top_left, bottom_left, bottom_right = table_coordinates
-    
+def displayBalls(pool_balls, img, radius):
+    """
+    Place the detected pool balls onto the pool table background.
+    """
     for pool_ball in pool_balls:
         x, y = pool_ball.x_cord, pool_ball.y_cord
         
-        cv2.circle(img, (x, y), radius, pool_ball.color, -1)     
-                
+        # Draw the cartoon ball on the pool table image
+        cv2.circle(img, (x, y), radius, pool_ball.color, -1)
+        
+        # Add the suit number (solid/striped) or similar identifier to the ball
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(img, str(pool_ball.suit), (x - 5, y + 5), font, 0.6, (255, 255, 255), 2, cv2.LINE_AA)
     
@@ -62,7 +63,7 @@ def showImgs(img, cartoon_img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def buildBorder(circles,min_radius):
+def buildBorder(circles, min_radius):
     pockets = []
     for (x, y, r) in circles:
             if r > 61: 
@@ -70,9 +71,43 @@ def buildBorder(circles,min_radius):
                 pockets.append(pocket)
     for pocket in pockets:
         print(pocket)
-            
 
-def cartoonify(table_coordinates, img=cv2.imread('data/pool_table_overhead.png')):
+def createPocketsFromEdges(edges):
+    """
+    Create the pocket positions from the given edges (top, bottom, left, right).
+    """
+    top_cords, bottom_cords, left_cords, right_cords = edges
+    # Extract corner points
+    top_left = top_cords[0]
+    top_right = top_cords[1]
+    bottom_left = bottom_cords[0]
+    bottom_right = bottom_cords[1]
+    
+    # Middle pocket positions (calculate center of top and bottom edges)
+    left_middle = ((left_cords[0][0] + left_cords[1][0]) // 2, (left_cords[0][1] + left_cords[1][1]) // 2)
+    right_middle = ((right_cords[0][0] + right_cords[1][0]) // 2, (right_cords[0][1] + right_cords[1][1]) // 2)
+    
+    # Pocket positions: corners + middle of left and right edges
+    pocket_positions = [top_left, top_right, bottom_left, bottom_right, left_middle, right_middle]
+    
+    return pocket_positions
+
+def addPoolTable(img, pocket_positions, edges, pool_ball_size):
+    """
+    Draw the modern pool table with pockets, using the edges passed.
+    """
+    top_left = tuple(edges[0][0])  # top-left corner
+    bottom_right = tuple(edges[3][1])  # bottom-right corner
+    table_color = (0, 128, 0)  # Green
+    cv2.rectangle(img, top_left, bottom_right, table_color, -1)
+    
+    pocket_radius = int(pool_ball_size * 2.5)
+    for pos in pocket_positions:
+        cv2.circle(img, pos, pocket_radius, (0, 0, 0), -1)  # Black Color
+
+    return img
+
+def cartoonify(img, edges):
     '''
     input: numpy.ndarray
     output: numpy.ndarray, [PoolBalls]
@@ -81,19 +116,27 @@ def cartoonify(table_coordinates, img=cv2.imread('data/pool_table_overhead.png')
     blurred = preprocess(img)
     circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=60, param1=50, param2=25, minRadius=20, maxRadius=30)
 
+        
+    top_edge = edges[0]
+    left_edge = edges[2]
+    WIDTH = top_edge[1][0] - top_edge[0][0]
+    HEIGHT = left_edge[1][1] - left_edge[0][1]
+    pocket_positions = createPocketsFromEdges(edges) 
+    
     img, pool_balls, avg_radius = plotCircles(img, circles)
+
+    blank_canvas = np.zeros((HEIGHT, WIDTH, 3), dtype="uint8")
+    cartoon_table = addPoolTable(blank_canvas, pocket_positions, edges, avg_radius)
+    cartoon_balls = displayBalls(pool_balls, cartoon_table, radius=avg_radius)
+    # showImgs(img, cartoon_img)  # Uncomment
     
-    cartoon_img = displayBalls(pool_balls, table_coordinates, img_shape=img.shape, radius=avg_radius)
+    return cartoon_balls, pool_balls, avg_radius, pocket_positions
     
-    # showImgs() # Comment out
-    
-    return cartoon_img, pool_balls, avg_radius
-    
-# Testingi
+# Testing
 if __name__ == '__main__':
     table_coordinates = np.float32([[415, 239], [172, 1121],
                        [1575, 1158], [1380, 243]])
     
-    cartoonify(table_coordinates)
-  
-    
+    cartoon_img, pool_balls, avg_radius = cartoonify(table_coordinates)
+    # If you need to save the image after cartoonification:
+    # cv2.imwrite("cartoonified_pool_table.png", cartoon_img)
--- src/backend/main.py
+++ src/backend/main.py
@@ -4,25 +4,32 @@ from Border import *
 from physics.simulatePaths import main  # We'll update simulatePaths soon
 import cv2
 
-def getCueTips(img):
+def getCueTips(img, run_sim):
     birds_eye_image, corners = getOutlineAndTransform(img, padding=40)
     print("getting cue tips")
     cropped_img, edges = getBorder(birds_eye_image)
     print("Got edges")
-    # print("got border")
-    # cartoon_img, pool_balls, avg_radius = cartoonify(corners, birds_eye_image)
 
-    paths = main(pool_balls, cue_ball=None, wall_cords=edges, ball_radius=avg_radius, show_simulation=False)
-    # Return paths in the future
-    return birds_eye_image
+    cartoon_img, pool_balls, avg_radius, pockets = cartoonify(birds_eye_image, edges)
+
+    tempfile_svg_name = ''
+    if run_sim:
+        tempfile_svg_name = main(pool_balls, pockets, cue_ball=None, wall_cords=edges, ball_radius=avg_radius, cue_angle=280, show_simulation=False)
+        
+    print("Stilll standing!!")
     
- 
+    return cartoon_img, tempfile_svg_name 
+
+
 if __name__ == '__main__':
     img = cv2.imread("data/pool_table_overhead.png")
-    birds_eye_image, corners = getOutlineAndTransform(img, padding=40)
-    cropped_img, edges = getBorder(birds_eye_image)
+    getCueTips(img, run_sim=False)
     
-    cartoon_img, pool_balls, avg_radius = cartoonify(corners, birds_eye_image)
+    
+    # birds_eye_image, corners = getOutlineAndTransform(img, padding=40)
+    # cropped_img, edges = getBorder(birds_eye_image)
+
+    # cartoon_img, pool_balls, avg_radius = cartoonify(corners, birds_eye_image)
 
     # cv2.imshow("cartoon", cartoon_img)
     # cv2.imshow("birds eye", birds_eye_image)
@@ -30,11 +37,9 @@ if __name__ == '__main__':
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
     # exit()
-    tempfile_svg_name = main(pool_balls, cue_ball=None, wall_cords=edges, ball_radius=avg_radius, cue_angle=280, show_simulation=False)
-    print(tempfile_svg_name)
+    # tempfile_svg_name = main(pool_balls, cue_ball=None, wall_cords=edges, ball_radius=avg_radius, cue_angle=280, show_simulation=False)
+    # print(tempfile_svg_name)
     
-    with open(tempfile_svg_name, 'r') as file:
-        svg_content = file.read()
+    # with open(tempfile_svg_name, 'r') as file:
+    #     svg_content = file.read()
 
-    print("SVG Content:")
-    print(svg_content)
