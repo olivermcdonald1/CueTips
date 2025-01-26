2add28e2fe62b08723749f575d4baa15db445aa1
Jacob <Jacob.leader06@gmail.com>
2025-01-25 21:20:49 -0500

    Delete CueBall.py

--- src/backend/CueBall.py
+++
@@ -1,7 +0,0 @@
-
-class CueBall:
-    def __init__(self, x_cord, y_cord, initial_speed_vector):
-        self.x_cord = x_cord
-        self.y_cord = y_cord
-        self.initial_speed_vector = initial_speed_vector
-        
