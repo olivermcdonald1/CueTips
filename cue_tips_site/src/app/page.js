48dc45ebc0ec64a7089c578b10f577135713d775
Jacob <Jacob.leader06@gmail.com>
2025-01-25 19:00:49 -0500

    Update page.js

--- cue_tips_site/src/app/page.js
+++ cue_tips_site/src/app/page.js
@@ -1,10 +1,13 @@
-import Link from 'next/link';
+'use client';
+import React from 'react';
+import Camera from '../components/camera';
 
 const Home = () => {
   return (
     
     <div className="font-cakra text-white opacity-100">
-      <h1 className="text-7xl text-center pt-10">Que Tips</h1>
+      <h1 className="text-7xl text-center pt-10">Cue Tips</h1>
+      <Camera/>
     </div>
 
   );
