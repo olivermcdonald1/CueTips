7b9b7b025914f51c12148ae592837730b9a32856
olivermcdonald1 <olivermcdonald10@gmail.com>
2025-01-25 20:40:38 -0500

    updated main

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,13 +1,19 @@
 from Cartoonify import *
 from ImageTo2d import *
+from Border import *
 import cv2
 
 if __name__ == '__main__':
-    birds_eye_image,corners = getOutlineAndTransform("data/pool_table_overhead.png", padding=40)
     
+    birds_eye_image,corners = getOutlineAndTransform("/Users/olivermcdonald/CueTips/data/pool_table_overhead.png", padding=40)
+   
     cartoon_img, pool_balls = cartoonify(corners,birds_eye_image)
+    
+   
+
+
 
     cv2.imshow("birds eye", birds_eye_image)
-    cv2.imshow("cartoon", cartoon_img)
+    #cv2.imshow("cartoon", cartoon_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
